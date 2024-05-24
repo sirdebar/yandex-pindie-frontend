a8df9f87d4668d738fdd101889b70a2dba70c7fa
sirdebar <semkochkin@gmail.com>
2024-05-24 12:00:32 +0300

    delete commmit

--- pindie/app/api/config.js
+++ pindie/app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://sirdebarback.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://sirdebarback.nomoredomainswork.ru';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
@@ -6,4 +6,4 @@ export const endpoints = {
   me: `${BASE_URL}/me`,
 }; 
 
-// added urls
+// added url
